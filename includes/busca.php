f4ef30f99e042e2bafc353ae4e6ee31f18b4dc51
Maycon Luczynski <mayconsantlu@gmail.com>
2014-07-28 12:32:16 -0300

    Alterado busca

--- includes/busca.php
+++ includes/busca.php
@@ -2,23 +2,26 @@
 if (isset($_POST['buscar']) && ($_POST['buscar'] != "") )
 	{
 		$busca 		= $_POST['buscar'];
+		echo '<div class="alert alert-info" role="alert">Buscando por: '.$busca.'</div>';
+		
+		#echo $busca.'<br />';
+
+		#$query = "Select * from paginas where titulo like '%$busca%' or conteudo like '%$busca%' order by desc;";
+		$query = "Select * from paginas where titulo like '%$busca%' or conteudo like '%$busca%';";
+		//$stmt = $conexao->query($query);
+		$stmt = $conexao -> prepare($query);
+		//$stmt -> bindValue("busca", $busca);
+		$stmt -> execute();
+		$resultado = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+		//print_r($resultado);
+		
+		foreach ($resultado as $result) {
+			echo $resultado[0]["titulo"];
+		}
+		
+		
 	}else {
 		echo '<div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign"></span> Desculpe o campo de pesquisa não pode ficar em branco!</div>';
 	}
 
-echo $busca.'<br />';
 
-#$query = "Select * from paginas where titulo like '%$busca%' or conteudo like '%$busca%' order by desc;";
-$query = "Select * from paginas where titulo like '%$busca%' or conteudo like '%$busca%' order by desc ;";
-//$stmt = $conexao->query($query);
-$stmt = $conexao -> prepare($query);
-//$stmt -> bindValue("busca", $busca);
-$stmt -> execute();
-$resultado = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-print_r($resultado);
-
-foreach ($resultado as $result) {
-	echo $resultado["titulo"];
-}
-
-//'%$pesquisar%' or desc_rapida like '%$pesquisar%' or tags like '%$pesquisar%' order by plano desc; "
